6bfb64a101998737821f13dac7403cf3e1eebc09
bansalvaibhav <vaibhav_ban331@yahoo.com>
2018-07-14 02:10:52 +0530

    My git project setup

---
+++ shoppingService/src/main/java/learn/vaibhav/shoppingService/App.java
@@ -0,0 +1,13 @@
+package learn.vaibhav.shoppingService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
